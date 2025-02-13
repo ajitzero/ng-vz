022353226e613e953d2edfe33a6c94daf7691447
Ajit <ajitzero@gmail.com>
2025-02-13 14:31:41 +0530

    feat: add analog for docs

---
+++ apps/docs/src/app/app.config.ts
@@ -0,0 +1,13 @@
+import { provideFileRouter, requestContextInterceptor } from '@analogjs/router';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideClientHydration } from '@angular/platform-browser';
+
+export const appConfig: ApplicationConfig = {
+	providers: [
+		provideZoneChangeDetection({ eventCoalescing: true }),
+		provideFileRouter(),
+		provideClientHydration(),
+		provideHttpClient(withFetch(), withInterceptors([requestContextInterceptor])),
+	],
+};
---
+++ apps/docs/src/app/pages/(home).page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import { AnalogWelcomeComponent } from './analog-welcome.component';
+
+@Component({
+	selector: 'docs-home',
+
+	imports: [AnalogWelcomeComponent],
+	template: `
+		<docs-analog-welcome />
+	`,
+})
+export default class HomeComponent {}
